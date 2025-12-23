3b2c1f8106afca7dcf5cbe37ad462a24fad63021
AtharvNiprul74 <atharvniprul74@gmail.com>
2025-12-24 00:16:48 +0530

    Backend Done

--- Day 8/React-FrontEnd/app/src/App.jsx
+++ Day 8/React-FrontEnd/app/src/App.jsx
@@ -1,10 +1,15 @@
-import NavBar from '../components/NavBar'
+import { Route, Router, Routes } from 'react-router'
 import './App.css'
+import Login from './pages/Login'
+import Home from './pages/Home'
 
 function App() {
   return (
     <>
-      <NavBar />
+      <Routes>
+        <Route path='/*' element={<Login/>}></Route>
+        <Route path='/home' element={<Home/>}></Route>
+      </Routes>
     </>
   )
 }
